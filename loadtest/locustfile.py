5a0c43da2047cab26035f9ce91b247f54b20f4b3
command <jawad.grar@gmail.com>
2025-01-13 11:36:39 -0500

    for simplicity limit to 1 rps per user

--- loadtest/locustfile.py
+++ loadtest/locustfile.py
@@ -1,6 +1,7 @@
-from locust import HttpUser, task
+from locust import HttpUser, task, constant_throughput
 
 class Products(HttpUser):
+    wait_time = constant_throughput(1) # 1 request per second
     @task
     def get_products(self):
         self.client.get("/api/products", headers={"Authorization": "Bearer todo_token"})
